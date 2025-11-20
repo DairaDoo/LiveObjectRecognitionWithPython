e3a0fb959eb284412f87521782dfe0ce2aa8860f
DairanDoo <your_email@example.com>
2025-11-19 21:58:53 -0400

    Implementación del modelo terminada, funciona tanto con imagenes como con cámara

--- main.py
+++ main.py
@@ -2,16 +2,94 @@ import numpy as np
 import cv2
 
 # definimos las rutas principales
-image_path = "imgs/sala_de_casa1.jpg"
-prototxt_path = "models/MobileNetSSD_deploy.prototxt"
-model_path = "models/mobilenet_iter_73000.caffemodelz"
+image_path = "imgs/calles_con_personas1.jpg"
+prototxt_path = "models/MobileNetSSD_deploy.prototxt.txt"
+model_path = "models/MobileNetSSD_deploy.caffemodel"
 
 # añadimos confianza mínima de 20%
 min_confidence = 0.2
 
-classes = ["background", "aeroplane", "bicycle", "bird", "boat",
-"bottle", "bus", "car", "cat", "chair", "cow", "diningtable"
-"dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+classes = [
+    "background",
+    "aeroplane",
+    "bicycle",
+    "bird",
+    "boat",
+    "bottle",
+    "bus",
+    "car",
+    "cat",
+    "chair",
+    "cow",
+    "diningtable",
+    "dog",
+    "horse",
+    "motorbike",
+    "person",
+    "pottedplant",
+    "sheep",
+    "sofa",
+    "train",
+    "tvmonitor",
+]
 
-np.random.seed(543210)
+# generar colores RGB para cada clase
+np.random.seed(543210)  # seed para mantener colores consistentes
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+
+# cargar el modelo preentrenado
+net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    __, image = cap.read()
+
+    # image = cv2.imread(image_path)
+    height, width = image.shape[0], image.shape[1]
+    blob = cv2.dnn.blobFromImage(image, 0.007843, (300, 300), 127.5)
+
+    net.setInput(blob)
+    detected_objects = net.forward()
+
+    for i in range(detected_objects.shape[2]):
+
+        confidence = detected_objects[0][0][i][2]
+
+        if confidence > min_confidence:
+
+            class_index = int(detected_objects[0, 0, i, 1])
+
+            upper_left_x = int(detected_objects[0, 0, i, 3] * width)
+            upper_left_y = int(detected_objects[0, 0, i, 4] * height)
+            lower_right_x = int(detected_objects[0, 0, i, 5] * width)
+            lower_right_y = int(detected_objects[0, 0, i, 6] * height)
+
+            prediction_text = f"{classes[class_index]}: {confidence:.2f}%"
+            cv2.rectangle(
+                image,
+                (upper_left_x, upper_left_y),
+                (lower_right_x, lower_right_y),
+                colors[class_index],
+                3,
+            )
+            cv2.putText(
+                image,
+                prediction_text,
+                (
+                    upper_left_x,
+                    upper_left_y - 15 if upper_left_y > 30 else upper_left_y + 15,
+                ),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.6,
+                colors[class_index],
+                2,
+            )
+
+            cv2.imshow("Objects Detected", image)
+            cv2.waitKey(10)
+
+
+cv2.destroyAllWindows()
+cap.release()
